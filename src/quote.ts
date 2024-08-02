14665543dc988d30af57ddf1018671a295971a09
FrierenDv <hackerxct007@gmail.com>
2024-08-02 21:28:02 +0700

    feat: Add quote and quoteExp functions for creating Markdown block quotes

---
+++ src/quote.ts
@@ -0,0 +1,15 @@
+import { _toMarkdown } from './_toMarkdown.js'
+import { Markdown } from './markdown.js'
+
+/**
+ * Creates a Markdown block quote.
+ * @example
+ * md.quote('foo_bar') // => Markdown with value '>foo\\_bar'
+ */
+export const quote = (text: unknown): Markdown => {
+  if (!text) {
+    return _toMarkdown()
+  }
+
+  return _toMarkdown(`>${_toMarkdown(text)}`, true)
+}
